d9b1e27415fd4f6a0bd9ac6c48c0a2016a0f519a
tonus-sebastian <52061104+tonus-sebastian@users.noreply.github.com>
2024-02-15 22:36:01 +0700

    Update 3-subnets.tf

--- 3-subnets.tf
+++ 3-subnets.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "private-us-east-2a" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.0.0/19"
+  cidr_block        = "10.0.0.0/24"
   availability_zone = "us-east-2a"
 
   tags = {
@@ -12,7 +12,7 @@ resource "aws_subnet" "private-us-east-2a" {
 
 resource "aws_subnet" "private-us-east-2b" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.32.0/19"
+  cidr_block        = "10.0.32.0/24"
   availability_zone = "us-east-2b"
 
   tags = {
@@ -24,7 +24,7 @@ resource "aws_subnet" "private-us-east-2b" {
 
 resource "aws_subnet" "public-us-east-2a" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.64.0/19"
+  cidr_block              = "10.0.64.0/24"
   availability_zone       = "us-east-2a"
   map_public_ip_on_launch = true
 
@@ -37,7 +37,7 @@ resource "aws_subnet" "public-us-east-2a" {
 
 resource "aws_subnet" "public-us-east-2b" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.96.0/19"
+  cidr_block              = "10.0.96.0/24"
   availability_zone       = "us-east-2b"
   map_public_ip_on_launch = true
 
